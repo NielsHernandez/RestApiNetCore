f79933f7951d31f238d1b40c0f0ca45f9b9ad002
niels <niels.hernandezflores@gmail.com>
2025-01-28 23:39:00 -0600

    setting up models and db init

---
+++ NZWalks.api/Models/Domain/Walk.cs
@@ -0,0 +1,31 @@
+﻿namespace NZWalks.api.Models.Domain
+{
+    public class Walk
+    {
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double LengthInKm { get; set; }
+
+        public string? WalkImageUrl { get; set; }
+
+        //this model will have FK relate on the DB
+
+        public Guid DifficultyId  { get; set; }
+
+
+        public Guid RegionId { get; set; }
+
+        //navigation properties
+
+        public Difficulty Difficulty { get; set; }
+
+        public Region Region { get; set; }
+
+
+    }
+}
