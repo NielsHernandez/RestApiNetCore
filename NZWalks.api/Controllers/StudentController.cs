c2c80b062acac38ffc5f785e3d73b1b27bc3d279
niels <niels.hernandezflores@gmail.com>
2025-01-28 22:41:47 -0600

    Add project files.

---
+++ NZWalks.api/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        [HttpGet]
+
+       public IActionResult GetAllStudents()
+        {
+
+            string[] students = new string[] {"niels","jacob","allen","mia","julieta"};
+
+            return Ok(students);
+        }
+    }
+}
